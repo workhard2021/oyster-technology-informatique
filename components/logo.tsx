c68c1b39c09125a52ab953bd1430ac2bf9211031
workhard2021 <csouleymane025@gmail.com>
2022-11-09 14:15:21 +0100

    update s

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,6 +1,6 @@
 export const LogoSection=({data}:any)=>{
     return (<div className="w-full z-0 relative mb-4">
-           <div className="w-full md:h-[650px] h-[300px] bg-url" style={{backgroundImage:`url(${data.image})`}}>
+           <div className="w-full md:h-[600px] h-[300px] bg-url" style={{backgroundImage:`url(${data.image})`}}>
                 <div className="relative md:top-[140px] top-[100px]">
                     <h2 className="text-center uppercase text-[#231942] md:text-[20px] text-[18px] py-4 text-title-shadow">{data.sub_title}</h2>
                     <h1 style={{fontWeight:'600'}} className="text-center text-[#231942] md:text-[3vw] text-title-shadow">{data.title}</h1>
