90d1835f49c2fa35a6fa825f715479de0e5b6dd8
workhard2021 <csouleymane025@gmail.com>
2022-11-14 19:00:34 +0100

    update

--- components/scrollCloud.tsx
+++ components/scrollCloud.tsx
@@ -31,14 +31,14 @@ export const ScrollCloud = ({data,title,sub_title}:any) => {
         <h2 className="w-full text-center mb-[35px] text-[35px] m-auto">{title}</h2>
         <div className="md:w-[50%] w-full text-center m-auto mb-[10px]">{sub_title}</div>
         <div className="relative py-4 m-auto md:w-[100%] w-full">
-        <div className="relative md:w-[80%] w-full bg-white m-auto">
+        <div className="relative md:w-[80%] w-full m-auto">
              <div className="absolute w-full top-[50%] h-0 m-auto flex z-20 rounded-md justify-between items-center">
-                <button onClick={(e) => prevImage(e)} className="p-1  rounded-md outline-none relative"><BiChevronLeft className="inline" size={70}/></button>
-                <button onClick={(e) => nextImage(e)} className="p-1  rounded-md outline-none relative"><BiChevronRight className="inline" size={70}/></button>
+                <button onClick={(e) => prevImage(e)} className="p-1  rounded-md outline-none relative"><BiChevronLeft className="inline text-gray-600" size={70}/></button>
+                <button onClick={(e) => nextImage(e)} className="p-1  rounded-md outline-none relative"><BiChevronRight className="inline text-gray-600" size={70}/></button>
              </div>
              <div id='scroll-partener' className="container-carousel-scroll">
                  {data && data.map((value:any, index:any) => {
-                 return <Link href={'/'} target={'_blank'} key={index} className='bg-white m-2 p-4 item-partener'>
+                 return <Link href={'/'} target={'_blank'} key={index} className='bg-white mx-4 p-2 item-partener shadow-sm'>
                       <div className="relative h-full w-full bg-url-partenaire" style={{backgroundImage:`url(${avatarFn(value.image)})`}} />
                   </Link>
                })}
@@ -51,10 +51,10 @@ export const ScrollCloud = ({data,title,sub_title}:any) => {
 ScrollCloud.defaultProps={
         data:[
            {id:1,image:'../images/partenaires/carrefour.png'},
-           {id:2,image:'../images/partenaires/orange.webp'},
+           {id:2,image:'../images/partenaires/orange-bank.png'},
            {id:3,image:'../images/partenaires/ista.png'},
            {id:4,image:'../images/partenaires/rsi.png'},
-           {id:5,image:'../images/partenaires/orange-logo.png'},
+           {id:5,image:'../images/partenaires/orange.png'},
            {id:6,image:'../images/partenaires/telecom.png'},
         ],
         title:'Ils nous font confiance',
